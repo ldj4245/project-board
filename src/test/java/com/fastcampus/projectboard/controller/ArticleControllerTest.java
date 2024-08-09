d2e14ac8c6f34c97e27f3147425abc2c7b88b3fe
idaejin <ldj4241@naver.com>
2024-08-09 13:17:07 +0900

    #14 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 6cba961 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고
    미리 테스트 메소드 업데이트

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -37,13 +37,14 @@ class ArticleControllerTest {
 
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 상세 페이지 - 정상 호출")
+    @Test
     public void given_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         //Given
 
         //When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -51,26 +52,28 @@ class ArticleControllerTest {
 
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
         //Given
 
         //When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
 
     }
 
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
         //Given
 
         //When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
 
     }
