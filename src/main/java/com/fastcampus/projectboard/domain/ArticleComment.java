4cf1f01697d50ab48eec1d5477aeeec6cb07bad9
idaejin <ldj4241@naver.com>
2024-08-10 18:26:57 +0900

    #19 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은
    `data.sql` 테스트 데이터도 모두 업데이트
    
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -30,18 +30,20 @@ public class ArticleComment extends AuditingFields {
 
 
     @Setter @ManyToOne(optional = false) private Article article; //게시글 (ID)
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; //유저 정보 (ID)
     @Setter @Column(nullable = false,length = 500) private String content; //본문
 
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article,UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article,UserAccount userAccount, String content) {
+        return new ArticleComment(article,userAccount, content);
     }
 
     @Override
