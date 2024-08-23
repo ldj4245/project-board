2d9e1fd138cc7d7bdf7c100571b8013e75299bac
idaejin <ldj4241@naver.com>
2024-08-23 14:44:40 +0900

    #42 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

--- src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
@@ -7,6 +7,7 @@ import com.fastcampus.projectboard.dto.ArticleCommentDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.repository.ArticleCommentRepository;
 import com.fastcampus.projectboard.repository.ArticleRepository;
+import com.fastcampus.projectboard.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,13 +16,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 
@@ -33,70 +31,74 @@ class ArticleCommentServiceTest {
 
     @Mock private ArticleRepository articleRepository;
     @Mock private ArticleCommentRepository articleCommentRepository;
+    @Mock private UserAccountRepository userAccountRepository;
 
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
-    void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments(){
-        //Given
+    void givenArticleId_whenSearchingArticleComments_thenReturnsArticleComments() {
+        // Given
         Long articleId = 1L;
         ArticleComment expected = createArticleComment("content");
         given(articleCommentRepository.findByArticle_Id(articleId)).willReturn(List.of(expected));
 
-        //when
+        // When
         List<ArticleCommentDto> actual = sut.searchArticleComments(articleId);
 
-        //Then
+        // Then
         assertThat(actual)
                 .hasSize(1)
-                .first().hasFieldOrPropertyWithValue("content",expected.getContent());
+                .first().hasFieldOrPropertyWithValue("content", expected.getContent());
         then(articleCommentRepository).should().findByArticle_Id(articleId);
     }
 
     @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
     @Test
-    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment(){
-        //Given
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
-        //When
+        // When
         sut.saveArticleComment(dto);
 
-        //Then
+        // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
     @DisplayName("댓글 저장을 시도했는데 맞는 게시글이 없으면, 경고 로그를 찍고 아무것도 안 한다.")
     @Test
-    void givenNonexistentArticle_whenSavingArticleComment_thenLogsSituationAndDoesNothing(){
-        //Given
+    void givenNonexistentArticle_whenSavingArticleComment_thenLogsSituationAndDoesNothing() {
+        // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleRepository.getReferenceById(dto.articleId())).willThrow(EntityNotFoundException.class);
 
-        //when
+        // When
         sut.saveArticleComment(dto);
 
-        //Then
+        // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).shouldHaveNoInteractions();
         then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
     @DisplayName("댓글 정보를 입력하면, 댓글을 수정한다.")
     @Test
-    void givenArticleCommentInfo_whenUpdatingArticleComment_thenUpdatesArticleComment(){
-        //Given
+    void givenArticleCommentInfo_whenUpdatingArticleComment_thenUpdatesArticleComment() {
+        // Given
         String oldContent = "content";
         String updatedContent = "댓글";
         ArticleComment articleComment = createArticleComment(oldContent);
         ArticleCommentDto dto = createArticleCommentDto(updatedContent);
         given(articleCommentRepository.getReferenceById(dto.id())).willReturn(articleComment);
 
-        //When
+        // When
         sut.updateArticleComment(dto);
 
-        //Then
+        // Then
         assertThat(articleComment.getContent())
                 .isNotEqualTo(oldContent)
                 .isEqualTo(updatedContent);
@@ -105,43 +107,34 @@ class ArticleCommentServiceTest {
 
     @DisplayName("없는 댓글 정보를 수정하려고 하면, 경고 로그를 찍고 아무 것도 안 한다.")
     @Test
-    void givenNonexistentArticleComment_whenUpdatingArticleComment_thenLogsWarningAndDoesNothing(){
-        //Given
+    void givenNonexistentArticleComment_whenUpdatingArticleComment_thenLogsWarningAndDoesNothing() {
+        // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleCommentRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
 
-        //when
+        // When
         sut.updateArticleComment(dto);
 
-        //Then
+        // Then
         then(articleCommentRepository).should().getReferenceById(dto.id());
     }
 
     @DisplayName("댓글 ID를 입력하면, 댓글을 삭제한다.")
     @Test
-    void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment(){
-        //Given
+    void givenArticleCommentId_whenDeletingArticleComment_thenDeletesArticleComment() {
+        // Given
         Long articleCommentId = 1L;
         willDoNothing().given(articleCommentRepository).deleteById(articleCommentId);
 
-        //When
+        // When
         sut.deleteArticleComment(articleCommentId);
 
-        //Then
+        // Then
         then(articleCommentRepository).should().deleteById(articleCommentId);
     }
 
 
-
-
-
-
-
-
-
-
-
-    private ArticleCommentDto createArticleCommentDto(String content){
+    private ArticleCommentDto createArticleCommentDto(String content) {
         return ArticleCommentDto.of(
                 1L,
                 1L,
@@ -154,11 +147,11 @@ class ArticleCommentServiceTest {
         );
     }
 
-    private UserAccountDto createUserAccountDto(){
+    private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
                 "uno",
                 "password",
-                "uno@gmail.com",
+                "uno@mail.com",
                 "Uno",
                 "This is memo",
                 LocalDateTime.now(),
@@ -168,25 +161,25 @@ class ArticleCommentServiceTest {
         );
     }
 
-    private ArticleComment createArticleComment(String content){
+    private ArticleComment createArticleComment(String content) {
         return ArticleComment.of(
-                Article.of(createUserAccount(),"title","content","hashtag"),
+                Article.of(createUserAccount(), "title", "content", "hashtag"),
                 createUserAccount(),
                 content
         );
     }
 
-    private UserAccount createUserAccount(){
+    private UserAccount createUserAccount() {
         return UserAccount.of(
                 "uno",
                 "password",
-                "uno@gmail.com",
+                "uno@email.com",
                 "Uno",
                 null
         );
     }
 
-    private Article createArticle(){
+    private Article createArticle() {
         return Article.of(
                 createUserAccount(),
                 "title",
