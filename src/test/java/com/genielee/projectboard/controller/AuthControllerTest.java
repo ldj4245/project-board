db98c7fc062a0dde8ed78969c4049047f2249709
idaejin <ldj4241@naver.com>
2024-12-01 15:00:01 +0900

    #51 - AuthControllerTest: 개선
    이 테스트는 실제 구현 코드인 controller가 없으므로
    `Void`대신 재활용 가능성을 고려하여 inner class로 변경해주었음.

--- src/test/java/com/genielee/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/genielee/projectboard/controller/AuthControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @Import(TestSecurityConfig.class)
 @DisplayName("View 컨트롤러 - 인증 ")
-@WebMvcTest(Void.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -33,5 +34,12 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
+
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위한 빈 컴포넌트 생성.
+     */
+
+    @TestComponent
+    static class EmptyController{}
 }
 
