e814851b9bc226116e32861320ea66cea1339d2b
idaejin <ldj4241@naver.com>
2024-12-29 13:06:23 +0900

    #100 - 어드민 프로젝트에서 사용할 data rest api 들에 id 필드를 오픈
    data rest가 자동 생성하는 api 는 id 값을 노출하지 않는 것이 기본 설정.
    이를 노출해준다.
    id 값은 어드민 프로젝트의 테이블에서 관리 목적으로 보여줄 예정.

--- src/main/java/com/genielee/projectboard/config/DataRestConfig.java
+++ src/main/java/com/genielee/projectboard/config/DataRestConfig.java
@@ -1,5 +1,8 @@
 package com.genielee.projectboard.config;
 
+import com.genielee.projectboard.domain.Article;
+import com.genielee.projectboard.domain.ArticleComment;
+import com.genielee.projectboard.domain.Hashtag;
 import com.genielee.projectboard.domain.UserAccount;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,11 @@ public class DataRestConfig {
     @Bean
     public RepositoryRestConfigurer repositoryRestConfigurer(){
         return RepositoryRestConfigurer.withConfig((config,cors) ->
-                config.exposeIdsFor(UserAccount.class)
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
         );
     }
 }
